c1f8a8e9c7d4228307f9613d6d8d1b890b3e8a30
thor_ <tyocom@verizon.net>
2020-05-18 17:42:32 -0400

    test commit

--- src/main/java/com/thor/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/thor/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.thor.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.thor.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
